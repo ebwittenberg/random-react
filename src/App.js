ee559f86fb0d7a664cdad68ae39f4cb82f6257d1
Eric Wittenberg <ebwittenberg@gmail.com>
2021-06-16 16:10:50 -0400

    Add fake table data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const tableData = [
+    {
+      shipmentId: 100,
+      status: "DELIVERED",
+      shippingSpeed: "1D",
+    },
+    {
+      shipmentId: 150,
+      status: "SHIPPED",
+      shippingSpeed: "2D",
+    },
+    {
+      shipmentId: 200,
+      status: "PENDING PICKUP",
+      shippingSpeed: "5D",
+    },
+    {
+      shipmentId: 250,
+      status: "PENDING PICKUP",
+      shippingSpeed: "5D",
+    },
+    {
+      shipmentId: 300,
+      status: "SHIPPED",
+      shippingSpeed: "5D",
+    },
+    {
+      shipmentId: 200,
+      status: "DELIVERED",
+      shippingSpeed: "5D",
+    },
+    {
+      shipmentId: 200,
+      status: "DELIVERED",
+      shippingSpeed: "5D",
+    },
+  ];
+  return <div className="App">{/* Add table here */}</div>;
 }
 
 export default App;
